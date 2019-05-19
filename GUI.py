dc44f0612cd30e648b6363bd93e0b6a8ec53cce0
dhruvdadhania <dhruvdadhania@gmail.com>
2019-05-18 21:35:25 -0400

    Allows nonconsecutive timing

--- GUI.py
+++ GUI.py
@@ -16,25 +16,38 @@ buttons = []
 labels = []
 
 startTime = 0
+prevTime = [0] * 5
+
+
+def update_time(val):
+    global prevTime
+    global startTime
+    prevTime[val] = (time.time() - startTime + prevTime[val])
+    labels[val].configure(text=round(prevTime[val] / 3600, 1))
+    prevTime[val] = round(prevTime[val])
+    startTime = time.time()
+    if buttons[val]['text'] == 'stop':
+        root.after(60000, lambda: update_time(val))
 
 
 def callback(val):
     global startTime
     if buttons[val]['text'] == 'stop':
         buttons[val].configure(text='start')
-        time_lapse = round((time.time() - startTime) / 3600, 1)
-        labels[val].configure(text=time_lapse)
+        update_time(val)
         return
     for button in buttons:
         button.configure(text='start')
     buttons[val].configure(text='stop')
     startTime = time.time()
+    update_time(val)
 
 
 for x in range(5):
     temp = Entry(lowerSection, highlightbackground=bgColor)
     temp.grid(row=x, column=1, padx=10, pady=10)
-    temp = Button(lowerSection, highlightbackground=bgColor, text='start', command=lambda val=x: callback(val), width = 10)
+    temp = Button(lowerSection, highlightbackground=bgColor, text='start',
+                  command=lambda val=x: callback(val), width=10)
     temp.grid(row=x, column=2, padx=10, pady=10)
     buttons.append(temp)
     temp = Label(lowerSection, highlightbackground=bgColor, text='0.0')
